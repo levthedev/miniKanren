fb6d5d70a8e43af6842bfdfbffa1c73ece4fbd74
Lev Kravinsky <kravinskylev@gmail.com>
2016-03-30 18:01:49 -0400

    basic unions working and tested

--- sketch2.rb
+++ sketch2.rb
@@ -1,46 +1,111 @@
 class Var
-  attr_accessor :name, :value  # => nil
+  attr_accessor :value
+end
+
+class Goal
+  attr_accessor :values
+
+  def initialize
+    @values = []
+  end
 end
 
 class K
   def self.run &block
-    find_value block.call fresh  # => []
+    goal = Goal.new
+    yield goal
+    find_values goal
   end
 
   def self.fresh
-    Var.new       # => #<Var:0x007fc61202e560>
+    Var.new
   end
 
   def self.union x, y
-    assign x, y if unassigned? x, y  # => 4, []
+    if unassigned? x, y
+      assign x, y
+    elsif x == y
+      true
+    else
+      "uhhh"
+    end
   end
 
   def self.unassigned? x, y
-    return true if x.class == Var || y.class == Var  # => true, true
+    if @branching && (x.class == Goal || y.class == Goal)
+      assign_goal x, y
+    elsif @branching
+      true
+    elsif empty_goal x, y
+      assign_goal x, y
+      false
+    elsif empty_var x, y
+      true
+    elsif !empty_goal x, y
+      unassign_goal x, y
+    end
+  end
+
+  def self.empty_var x, y
+    x.class == Var && x.value.nil? ||  y.class == Var && y.value.nil?
+  end
+
+  def self.empty_goal x, y
+    (x.class == Goal && x.values.empty?) ||  (y.class == Goal && y.values.empty?)
   end
 
   def self.assign x, y
-    if x.class == Var && x.value.nil?     # => true, false
-      var, value = x, y                   # => [#<Var:0x007fc61202e560>, 4]
-      var.value = value                   # => 4
-    elsif y.class == Var && y.value.nil?  # => false
+    if x.class == Var
+      var, value = x, y
+      var.value = value
+    elsif y.class == Var
       var, value = y, x
       var.value = value
+    end
+  end
+
+  def self.assign_goal x, y
+    if x.class == Goal
+      goal, var = x, y
+      goal.values << (find_value var)
+    elsif y.class == Goal
+      goal, var = y, x
+      goal.values << (find_value var)
+    end
+  end
+
+  def self.unassign_goal x, y
+    if x.class == Goal
+      x.values = []
+    elsif y.class == Goal
+      y.values = []
+    end
+  end
+
+  def self.find_values maybe_goal
+    maybe_goal
+    if maybe_goal.class == Goal
+      maybe_goal.values.map do |value|
+        find_value value
+      end
     else
-      []                                  # => []
-    end                                   # => 4, []
+      maybe_goal
+    end
   end
 
   def self.find_value maybe_var
-    if maybe_var.class == Var && maybe_var.value.class == Var  # => false
+    if maybe_var.class == Var && maybe_var.value.class == Var
       find_value maybe_var.value
     else
-      maybe_var.class == Var ? maybe_var.value : maybe_var     # => []
-    end                                                        # => []
+      maybe_var.class == Var ? maybe_var.value : maybe_var
+    end
+  end
+
+  def self.cond *branches
+    @branching = true
+    branches.map do |branch|
+      branch.call
+    end
+    @branching = false
   end
 end
-
-K.run do |y|     # => K
-  K.union(y, 4)  # => 4
-  K.union(y, 5)  # => []
-end              # => []
--- spec/sketch2_test.rb
+++ spec/sketch2_test.rb
@@ -1,39 +1,79 @@
-gem 'minitest', '~> 5.0'
-require "minitest/autorun"
-require "minitest/pride"
-require_relative "../sketch2.rb"
+gem 'minitest', '~> 5.0'          # => true
+require "minitest/autorun"        # => true
+require "minitest/pride"          # => true
+require_relative "../sketch2.rb"  # => true
 
 class KanrenTest < Minitest::Test
   def test_it_has_basic_unions
-    goal = K.run do |q|
-      z = "hi"
-      K.union q, z
-    end
+    goal = K.run do |q|            # => K
+      z = "hi"                     # => "hi"
+      K.union q, z                 # => nil
+    end                            # => nil
 
-    assert_equal("hi", goal)
+    assert_equal(["hi"], goal)
   end
 
   def test_it_has_union_chains
-    goal = K.run do |q|
-      x = K.fresh
-      z = "hi"
-      K.union x, z
-      K.union z, q
-    end
+    goal = K.run do |q|         # => K
+      x = K.fresh               # => #<Var:0x007fbb9a161ab8>
+      z = "hi"                  # => "hi"
+      K.union x, z              # => "hi"
+      K.union z, q              # => nil
+    end                         # => nil
 
-    assert_equal("hi", goal)
+    assert_equal(["hi"], goal)
   end
 
   def test_it_recursively_returns_unions
-    goal = K.run do |q|
-      x = K.fresh
-      z = K.fresh
-      K.union x, z
-      K.union 3, z
-      K.union q, x
-      q
+    goal = K.run do |q|                   # => K
+      x = K.fresh                         # => #<Var:0x007fbb9a1530a8>
+      z = K.fresh                         # => #<Var:0x007fbb9a152e78>
+      K.union x, z                        # => #<Var:0x007fbb9a152e78>
+      K.union 3, z                        # => 3
+      K.union q, x                        # => nil
+      q                                   # => #<Goal:0x007fbb9a1530f8 @values=[#<Var:0x007fbb9a152e78 @value=3>]>
+    end                                   # => [3]
+
+    assert_equal([3], goal)  # => true
+  end
+
+  def test_it_rejects_contradictory_unions
+    goal = K.run do |y|
+      K.union(4, y)
+      K.union("hi", y)
     end
 
-    assert_equal(3, goal)
+    assert_equal([], goal)
+  end
+
+  def test_it_has_basic_conds
+    goal = K.run do |y|
+      case_1 = lambda { K.union(4, y) }
+      case_2 = lambda { K.union("hi", y) }
+      K.cond(case_1, case_2)
+    end
+
+    assert_equal([4, "hi"], goal)
   end
 end
+
+# >> Run options: --seed 11903
+# >>
+# >> # Running:
+# >>
+# >> [41m[37mF[0m[41m[37mF[0m[38;5;154m.[0m
+# >>
+# >> [38;5;154mF[0m[38;5;154ma[0m[38;5;148mb[0m[38;5;184mu[0m[38;5;184ml[0m[38;5;214mo[0m[38;5;214mu[0m[38;5;208ms[0m[38;5;208m [0m[38;5;203mr[0m[38;5;203mu[0m[38;5;198mn[0m in 0.001250s, 2400.2573 runs/s, 2400.2573 assertions/s.
+# >>
+# >>   1) Failure:
+# >> KanrenTest#test_it_has_union_chains [/Users/levkravinsky/Desktop/miniKanren/spec/sketch2_test.rb:24]:
+# >> Expected: ["hi"]
+# >>   Actual: nil
+# >>
+# >>
+# >>   2) Failure:
+# >> KanrenTest#test_it_has_basic_unions [/Users/levkravinsky/Desktop/miniKanren/spec/sketch2_test.rb:13]:
+# >> Expected: ["hi"]
+# >>   Actual: nil
+# >>
+# >> 3 runs, 3 assertions, 2 failures, 0 errors, 0 skips
