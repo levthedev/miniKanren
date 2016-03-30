2772b73fed4d5d272783c74e6036dd1ac932c853
Lev Kravinsky <kravinskylev@gmail.com>
2016-03-30 16:46:48 -0400

    basic union tests and functionality

--- sketch2.rb
+++ sketch2.rb
@@ -1,38 +1,46 @@
 class Var
-  attr_accessor :name, :value
+  attr_accessor :name, :value  # => nil
 end
 
 class K
   def self.run &block
-    find_value block.call fresh
+    find_value block.call fresh  # => []
   end
 
   def self.fresh
-    Var.new
+    Var.new       # => #<Var:0x007fc61202e560>
   end
 
   def self.union x, y
-    assign x, y if unassigned? x, y
+    assign x, y if unassigned? x, y  # => 4, []
   end
 
   def self.unassigned? x, y
-    return true if x.class == Var || y.class == Var
+    return true if x.class == Var || y.class == Var  # => true, true
   end
 
   def self.assign x, y
-    if x.class == Var
-      var, value = x, y
-    else
+    if x.class == Var && x.value.nil?     # => true, false
+      var, value = x, y                   # => [#<Var:0x007fc61202e560>, 4]
+      var.value = value                   # => 4
+    elsif y.class == Var && y.value.nil?  # => false
       var, value = y, x
-    end
-    var.value = value
+      var.value = value
+    else
+      []                                  # => []
+    end                                   # => 4, []
   end
 
   def self.find_value maybe_var
-    if maybe_var.class == Var && maybe_var.value.class == Var
+    if maybe_var.class == Var && maybe_var.value.class == Var  # => false
       find_value maybe_var.value
     else
-      maybe_var.class == Var ? maybe_var.value : maybe_var
-    end
+      maybe_var.class == Var ? maybe_var.value : maybe_var     # => []
+    end                                                        # => []
   end
 end
+
+K.run do |y|     # => K
+  K.union(y, 4)  # => 4
+  K.union(y, 5)  # => []
+end              # => []
